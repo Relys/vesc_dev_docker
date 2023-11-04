544edf5ad0c4530ed67d2cfa815ffc5264f27491
Syler Clayton <2224238+Relys@users.noreply.github.com>
2023-11-03 18:16:58 -0700

    Update get_vesc_dado.sh

--- get_vesc_dado.sh
+++ get_vesc_dado.sh
@@ -6,6 +6,7 @@ cd /
 git clone https://github.com/surfdado/vesc_tool.git
 cd /vesc_tool
 git checkout bms_state
+sed -i 's/bms_fault_state/bms_fault_code/g' datatypes.h #fix typo
 cd /
 git clone https://github.com/surfdado/vesc_pkg.git
 cd /vesc_pkg
