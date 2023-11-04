e831854ad685b81b5a12b1fee6cd1aa16b22c05a
Syler Clayton <2224238+Relys@users.noreply.github.com>
2023-11-04 01:33:37 -0700

    won't copy over scripts if changed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo chmod 777 /vesc_dev
-sudo cp /vesc_dev_scripts/* /vesc_dev
+sudo cp -n /vesc_dev_scripts/* /vesc_dev
 #Xvfb :99 &
 #export DISPLAY=:99
 #export XDG_RUNTIME_DIR=/tmp/runtime-root
